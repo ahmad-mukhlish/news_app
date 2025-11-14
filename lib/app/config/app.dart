bcbdab15b327a358bae28d0c03cebe7544a1e04e
ahmad-mukhlish <ahmad.mukhlish.saputra@gmail.com>
2025-11-14 18:16:35 +0700

    fix news flows

--- lib/app/config/app.dart
+++ lib/app/config/app.dart
@@ -1,4 +1,3 @@
-import 'package:chucker_flutter/chucker_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import '../../features/main/presentation/get/main_binding.dart';
@@ -25,7 +24,6 @@ class App extends StatelessWidget {
       ),
       home: const MainScreen(),
       initialBinding: MainBinding(),
-      navigatorObservers: [ChuckerFlutter.navigatorObserver],
     );
   }
 }
